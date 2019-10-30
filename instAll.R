59c1897104a4556c2fbf519c9021503400b206a8
Oliver D. Reithmaier <45456001+K4tana@users.noreply.github.com>
2019-10-30 21:53:05 +0100

    Create instAll.R
    Can install its own dependencies and deals with the common repos. Will modify to make tidier code as soon as i learn to botch together function names.

---
+++ instAll.R
@@ -0,0 +1,42 @@
+instAll <- function(x, repo=NULL) {
+  `%notin%` <- Negate(`%in%`)
+  if("remotes" %notin% rownames(installed.packages()))
+    {
+    install.packages("remotes")
+    }
+    library(remotes)
+    if(x %in% rownames(installed.packages()))
+    {
+      cat("Package is already installed")
+    }
+    else
+    {
+      if(is.null(repo))
+      {
+        install.packages(x)
+        if(x %notin% rownames(installed.packages()))
+        {
+          warning("Package not found. Please specify source or use other functions from the remotes package!")
+          stop()
+        }
+      }
+      else
+        {
+          if(repo=="github")
+          {
+            install_github(x)
+          }
+          else
+            {
+              if(repo=="bitbucket")
+              {
+                install_bitbucket(x)
+              }
+              else
+              {
+                warning("Package not found. Please specify source or use other functions from the remotes package!")
+              }
+            }
+        }
+    }
+}
