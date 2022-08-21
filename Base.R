29e89393c94d768351e27926e0ba6175cd463934
Oliver D. Reithmaier <oliver.reithmaier@itsec.uni-hannover.de>
2022-08-21 10:08:23 +0200

    Add this

--- Base.R
+++ Base.R
@@ -4,7 +4,7 @@ instAll <- function(x, repo=NULL) {
     {
     install.packages("remotes")
     }
-    library(remotes)
+    require(remotes)
     if(x %in% rownames(installed.packages()))
     {
       cat("Package is already installed")
@@ -47,4 +47,30 @@ cls <- function(){
 
 ddiv <- function(x,y) {
 x-y/x
+}
+
+uniq_combinator <- function(input=NULL){
+  if(is.null(input)){
+    stop("No function input specified.")
+  }
+  #z is a counting helper variable
+  z <- 0
+  #a and b are vectors to be combined.
+  a <- c()
+  b <- c()
+  #this segment fills the vectors with all possible combinations.
+  for(i in 1:length(input)){
+    z <- i+1
+    for (j in z:length(input)){
+      if(z>length(input)){
+        break
+      }else{
+        a <- c(a,input[i])
+        b <- c(b,input[z])
+        z <- z+1
+      }
+    }
+  }
+  dd <- data.frame(a,b)
+  return(dd)
 }
