2e11f4308c0e8aff6da971809b283c682d5d2b40
Oliver D. Reithmaier <oliver.reithmaier@itsec.uni-hannover.de>
2022-08-23 11:01:33 +0200

    Added input checking.

--- neigh_combinator.R
+++ neigh_combinator.R
@@ -1,5 +1,9 @@
 #This function looks at a vector of elements and outputs all neighbor value combinations. #Works downstream from vector start and is an extension for expand.grid, written in base R. 
-neigh_combinator <- function(input){
+neigh_combinator <- function(input=NULL){
+  if(is.null(input)==T){
+    warning("No function input specified for combining.")
+    stop()
+  }
   a <- data.frame()
   for (i in 1:length(input)){
     if (i==length(input)){
